d8f38146275034ec5de244435a94ac10d51dcbe3
Yasin Acar <yasinaacar@outlook.com>
2024-04-10 17:32:19 +0300

    database moved to local from cloud system

--- startup/logger.js
+++ startup/logger.js
@@ -5,9 +5,7 @@ require("winston-mongodb");
 
 const config=require("config");
 
-const username= config.get("db.username");
-const password= config.get("db.password");
-const databasename= config.get("db.databasename")
+
 
 const logger=createLogger({
     level: "debug", //log debug
@@ -18,14 +16,14 @@ const logger=createLogger({
         new transports.Console(),//prints debug errors to the console
         new transports.File({filename: "logs/logs.log", level:"error"}),//create file name of "logs.log" for level type error
         new transports.File({filename: "logs/exceptions.log", level:"error", handleExceptions: true, handleRejections: true, maxFiles:"7d"}),
-        new transports.MongoDB({//save error to database if level type is error
-            level: "error",
-            db: `mongodb+srv://${username}:${password}@cluster0.slrguhg.mongodb.net/${databasename}?retryWrites=true&w=majority`,
-            options:{
-                useUnifiedTopology: true,
-            },
-            collection: "server_logs"//schema name
-        })
+    //     new transports.MongoDB({//save error to database if level type is error
+    //         level: "error",
+    //         db: `mongodb+srv://${username}:${password}@cluster0.slrguhg.mongodb.net/${databasename}?retryWrites=true&w=majority`,
+    //         options:{
+    //             useUnifiedTopology: true,
+    //         },
+    //         collection: "server_logs"//schema name
+    //     })
     ]
 });
 module.exports=logger;
